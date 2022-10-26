b89593e707ed5efd193594175519dbdf5c4f8f38
Toby Inkster <mail@tobyinkster.co.uk>
2022-10-26 16:40:15 +0100

    Tabs ftw

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,25 @@
 /// Retrieve the greeting that will be printed by the app.
 fn get_greeting() -> String {
-    "Hello, world!".to_string()
+	"Hello, world!".to_string()
 }
 
 /// Gives a friendly hello!
 fn main() {
-    println!("{}", get_greeting());
+	println!("{}", get_greeting());
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+	use super::*;
 
-    #[test]
-    fn test_get_greeting() {
-        assert_eq!(13, get_greeting().len());
-    }
+	#[test]
+	fn test_get_greeting() {
+		assert_eq!(13, get_greeting().len());
+	}
 
-    #[test]
-    fn test_main() {
-        main();
-        assert!(true);
-    }
+	#[test]
+	fn test_main() {
+		main();
+		assert!(true);
+	}
 }
